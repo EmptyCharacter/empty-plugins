cb81922a965fe5372ea24da8c2f4d6f6eda87590
Joshua-Farin <102901313+Joshua-Farin@users.noreply.github.com>
2022-07-28 12:12:59 -1000

    init

---
+++ empty-Motherlode/src/main/java/dev/empty/scripts/motherlode/tasks/MineOre.java
@@ -0,0 +1,48 @@
+package dev.empty.scripts.motherlode.tasks;
+
+import net.unethicalite.api.entities.NPCs;
+import net.unethicalite.api.movement.Movement;
+import net.unethicalite.api.movement.Reachable;
+import net.unethicalite.api.widgets.Dialog;
+import net.runelite.api.DialogOption;
+import net.runelite.api.NPC;
+
+public class MineOre implements ScriptTask
+{
+	@Override
+	public boolean validate()
+	{
+		return false;
+	}
+
+	@Override
+	public int execute()
+	{
+		if (Movement.isWalking())
+		{
+			return 1000;
+		}
+
+		NPC karim = NPCs.getNearest("Karim");
+		if (karim == null)
+		{
+			Movement.walkTo(3274, 3181, 0);
+			return 1000;
+		}
+
+		if (!Reachable.isInteractable(karim))
+		{
+			Movement.walkTo(karim);
+			return 1000;
+		}
+
+		Dialog.invokeDialog(
+				DialogOption.NPC_CONTINUE,
+				DialogOption.CHAT_OPTION_TWO,
+				DialogOption.PLAYER_CONTINUE
+		);
+
+		karim.interact("Talk-to");
+		return 300;
+	}
+}
