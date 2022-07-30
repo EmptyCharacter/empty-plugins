de038a13b4b183c85fdf0f694f7a9b4f884c67cc
EmptyCharacter <farin.k.joshua@gmail.com>
2022-07-30 06:41:30 -1000

    Remove broken blugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,17 @@
 rootProject.name = "empty-plugins"
 
-include("empty-EvilDragons")
-include("empty-Motherlode")
-include("empty-NightmareZone")
-include("empty-CooksAssistant")
-include("empty-MooBank")
-include("empty-ScriptTemplate")
-include("empty-LavaDragons")
-include("empty-Overload")
-include("empty-RockCake")
+//include("empty-EvilDragons")
+//include("empty-Motherlode")
+//include("empty-NightmareZone")
+//include("empty-CooksAssistant")
+//include("empty-MooBank")
+//include("empty-ScriptTemplate")
+//include("empty-LavaDragons")
+//include("empty-Absorption")
+//include("empty-RockCake")
 include("empty-PrayerFlicker")
-include("empty-Absorption")
+include("empty-Overload")
+
 
 
 
